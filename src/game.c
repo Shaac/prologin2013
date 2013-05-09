eba275a428e500ceaddd10e83f620230df4499ea
Shaac <sacha.delanoue@gmail.com>
2013-05-09 14:53:31 +0200

    One function per age

--- src/game.c
+++ src/game.c
@@ -2,6 +2,18 @@
 
 static int age = 1; /**< There are different ages (steps) through the game. */
 
+static void game_age_1(void)
+{
+}
+
+static void game_age_2(void)
+{
+}
+
+static void game_age_3(void)
+{
+}
+
 /**
  * @brief Function called each turn.
  */
@@ -9,6 +21,13 @@ void game_play()
 {
     switch (age) {
         case 1: // Colonisation age.
+            game_age_1();
+            break;
+        case 2: // Construction age.
+            game_age_2();
+            break;
+        case 3: // Gold age.
+            game_age_3();
             break;
     }
 }
