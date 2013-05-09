9eca8089fc903c9cacbcb8ad4eead155945d4c71
Shaac <sacha.delanoue@gmail.com>
2013-05-09 14:51:06 +0200

    Line too long

--- src/game.c
+++ src/game.c
@@ -1,6 +1,6 @@
 #include "game.h"
 
-static int age = 1; /**< There are different ages (= steps) through the game. */
+static int age = 1; /**< There are different ages (steps) through the game. */
 
 /**
  * @brief Function called each turn.
