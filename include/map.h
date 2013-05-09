e25200d0faddba69407b3ed1fe8e79c60eaa939b
Shaac <sacha.delanoue@gmail.com>
2013-05-09 21:17:04 +0200

    Comment map.h

--- include/map.h
+++ include/map.h
@@ -1,13 +1,24 @@
 #ifndef __MAP_H__
 #define __MAP_H__
 
-extern int             map_isles_number;
-extern struct Position *map_isles;
-extern int             **map_danger;
-extern int             **map_proximity;
+extern int             map_isles_number; /**< Number of isles and volcanos. */
+extern struct Position *map_isles;       /**< Their position on the map. */
+extern int             **map_danger;     /**< Number of menacing ships. */
+extern int             **map_proximity;  /**< Same, with isles counting. */
 
+/**
+ * @brief Init the map structures and make some pre calculations.
+ */
 void map_init(void);
+
+/**
+ * @brief Refresh the map with nez data from this turn.
+ */
 void map_refresh(void);
+
+/**
+ * @brief Free the data structures.
+ */
 void map_clean(void);
 
 #endif // __MAP_H__
