aee8bb1e85ede33396eab856adbb2f26765232c7
Shaac <sacha.delanoue@gmail.com>
2013-05-10 04:26:48 +0200

    Cleverer attacks

--- src/map.c
+++ src/map.c
@@ -157,17 +157,33 @@ void map_go_to(struct Ship ship, struct Position pos)
     }
 }
 
+static int map_force(int x, int y, bool mine)
+{
+    int ret = 0;
+    struct Ship_array ships = api_ship_list(x, y);
+    for (unsigned int i = 0; i < ships.length; i ++)
+        if (ships.ships[i].type == SHIP_GALLEON) {
+            if (mine && ships.ships[i].movable)
+                ret++;
+            if (!mine && ships.ships[i].player == other)
+                ret++;
+            if (!mine && ships.ships[i].player == me)
+                ret--;
+        }
+    return ret;
+}
+
 void map_move_to_front(struct Ship ship)
 {
     // See if we can attack.
-    int max = 0;
+    int force = map_force(ship.pos.x, ship.pos.y, true);
     struct Position p = {-1, -1};
-    FOR_i_j_IN_SURROUNDING(ship.pos.x, ship.pos.y, GALLEON_MOVEMENT)
-        if (abs(i - ship.pos.x) + abs(j - ship.pos.y) > 0)
-            if (map_proximity[i][j] > max) {
-                max = map_proximity[i][j];
-                p = (struct Position) {i, j};
-            }
+    FOR_i_j_IN_SURROUNDING(ship.pos.x, ship.pos.y, GALLEON_MOVEMENT) {
+        int ennemies = map_force(i, j, false); // TODO optimize
+        if (ennemies > 0 && force > ennemies) {
+            p = (struct Position) {i, j};
+        }
+    }
     if (p.x != -1)
         map_go_to(ship, p);
 
