1779ea27b0809e99057f4ef7f928f9f940bb83e8
Shaac <sacha.delanoue@gmail.com>
2013-05-11 00:23:46 +0200

    Add some comments

--- movements.c
+++ movements.c
@@ -3,11 +3,84 @@
 #include "game.h"
 #include "fleet.h"
 
-static struct Position help;
+static struct Position help; /**< A caravel can ask for assistance there. */
+
+/**
+ * @brief Get the military force at a position.
+ *
+ * @param x    The position.
+ * @param y    The position.
+ * @param mode If 1, count only my movable ships; if 2 count the ennemy forces,
+ * taking into account my own forces already there. if 3 count my forces only.
+ */
+static int movements_strength(int x, int y, int mode)
+{
+    int ret = 0;
+    struct Ship_array ships = api_ship_list(x, y);
+    for (unsigned int i = 0; i < ships.length; i ++)
+        if (ships.ships[i].type == SHIP_GALLEON) {
+            if (mode == 1 && ships.ships[i].movable)
+                ret++;
+            if (mode == 2 && ships.ships[i].player == other)
+                ret++;
+            if (mode == 2 && ships.ships[i].player == me)
+                ret--;
+            if (mode == 3 && ships.ships[i].player == me)
+                ret++;
+        }
+    free(ships.ships);
+    return ret;
+}
+
+/**
+ * @brief Move a galleon in an offensive way.
+ *
+ * @param ship The galleon.
+ */
+static void movements_move_to_front(struct Ship ship)
+{
+    // See if we can attack.
+    int force = movements_strength(ship.pos.x, ship.pos.y, 1);
+    struct Position p = {-1, -1};
+    FOR_i_j_IN_SURROUNDING(ship.pos.x, ship.pos.y, GALLEON_MOVEMENT) {
+        int ennemies = movements_strength(i, j, 2) + api_isle_owner(
+                (struct Position) {i, j}) == other; // TODO optimize
+        if (ennemies > 0 && force >= ennemies) {
+            p = (struct Position) {i, j};
+        } else if (ennemies > 0 && p.x < 0)
+            p.x = -2; // Signal that we are near the action.
+    }
+    if (p.x != -1) {
+        movements_go_to(ship, p);
+        return;
+    } else if (p.x == -2) { // avoid suicidal attacks
+        movements_flee(ship);
+        return;
+    }
+
+    // If we can not attack, go near the ennemy.
+    // We will search arround this position in circles.
+    // TODO Make this stop sooner.
+    for (int diff = 1; diff < 2 * FIELD_SIZE; diff++)
+        for (int dx = -diff; dx <= diff; dx++)
+            for (int dy = abs(dx) - diff; true; dy += 2 * (diff - abs(dx))) {
+                p = (struct Position) {ship.pos.x + dx, ship.pos.y + dy};
+                if (p.x >= 0 && p.y >= 0 && p.x < FIELD_SIZE && p.y <
+                        FIELD_SIZE) {
+                    if (map_positions[p.x][p.y] &&
+                            movements_strength(p.x, p.y, 2) >= 0) {
+                        // We need some force here.
+                        movements_go_to(ship, p);
+                        return;
+                    }
+                }
+                if (dy == diff - abs(dx))
+                    break;
+            }
+}
 
 void movements_go_to(struct Ship ship, struct Position pos)
 {
-    // TODO check if api_move is not bugged (circulary issues).
     if (api_move(ship.id, pos) != OK) { // If we can not go directly.
         int movement = ship.type == SHIP_GALLEON ? GALLEON_MOVEMENT :
             CARAVEL_MOVEMENT;
@@ -27,65 +100,6 @@ void movements_go_to(struct Ship ship, struct Position pos)
     }
 }
 
-int movements_force(int x, int y, int mode)
-{
-    int ret = 0;
-    struct Ship_array ships = api_ship_list(x, y);
-    for (unsigned int i = 0; i < ships.length; i ++)
-        if (ships.ships[i].type == SHIP_GALLEON) {
-            if (mode == 1 && ships.ships[i].movable)
-                ret++;
-            if (mode == 2 && ships.ships[i].player == other)
-                ret++;
-            if (mode == 2 && ships.ships[i].player == me)
-                ret--;
-            if (mode == 3 && ships.ships[i].player == me)
-                ret ++;
-        }
-    free(ships.ships);
-    return ret;
-}
-
-#include <stdio.h>
-void movements_move_to_front(struct Ship ship)
-{
-    // See if we can attack.
-    int force = movements_force(ship.pos.x, ship.pos.y, 1);
-    struct Position p = {-1, -1};
-    FOR_i_j_IN_SURROUNDING(ship.pos.x, ship.pos.y, GALLEON_MOVEMENT) {
-        int ennemies = movements_force(i, j, 2) + api_isle_owner(
-                (struct Position) {i, j}) == other; // TODO optimize
-        if (ennemies > 0 && force >= ennemies) {
-            p = (struct Position) {i, j};
-        } else if (ennemies > 0 && p.x < 0)
-            p.x = -2;
-    }
-    if (p.x != -1) {
-        movements_go_to(ship, p);
-        return;
-    } else if (p.x == -2) { // avoid suicidal attacks
-        movements_flee(ship); // TODO regroup
-        return;
-    }
-
-    // If not, go near the ennemy.
-    // TODO make this better
-    for (int diff = 1; diff < 2 * FIELD_SIZE; diff++)
-        for (int dx = -diff; dx <= diff; dx++)
-            for (int dy = abs(dx) - diff; true; dy += 2 * (diff - abs(dx))) {
-                p = (struct Position) {ship.pos.x + dx, ship.pos.y + dy};
-                if (p.x >= 0 && p.y >= 0 && p.x < FIELD_SIZE && p.y <
-                        FIELD_SIZE) {
-                    if (map_positions[p.x][p.y] && movements_force(p.x, p.y, 2) >= 0) {
-                        movements_go_to(ship, p);
-                        return;
-                    }
-                }
-                if (dy == diff - abs(dx))
-                    break;
-            }
-}
-
 void movements_flee(struct Ship ship)
 {
     int r = ship.type == SHIP_GALLEON ? GALLEON_MOVEMENT : CARAVEL_MOVEMENT;
@@ -93,7 +107,7 @@ void movements_flee(struct Ship ship)
     struct Position p = {-1, -1};
     FOR_i_j_IN_SURROUNDING(ship.pos.x, ship.pos.y, r)
         if (!map_danger[i][j]) {
-            int nb = movements_force(i, j, 3);
+            int nb = movements_strength(i, j, 3); // Try to group up.
             if (nb > max) {
                 max = nb;
                 p = (struct Position) {i, j};
@@ -101,7 +115,6 @@ void movements_flee(struct Ship ship)
         }
     if (p.x != -1)
         movements_go_to(ship, p);
-    return;
 }
 
 void movements_retreive_gold()
@@ -199,7 +212,7 @@ void movements_move_galleons()
             struct Ship ship = api_get_ship(fleet_galleons[i]);
             if (ship.movable) {
                 if (help.x != -1 && api_move(ship.id, help) == OK) {
-                    if (-movements_force(help.x, help.y, 2) >
+                    if (-movements_strength(help.x, help.y, 2) >
                             map_danger[help.x][help.y])
                         help.x = -1;
                 } else
@@ -207,5 +220,4 @@ void movements_move_galleons()
             }
         }
     }
-
 }
--- movements.h
+++ movements.h
@@ -7,16 +7,9 @@
  * @brief Go to the direction of a given position.
  *
  * @param ship The ship to move.
- * @param pos  The postion to move to.
+ * @param pos  The position to move to.
  */
 void movements_go_to(struct Ship ship, struct Position pos);
-
-/**
- * @brief Move a galleon in an offesive way.
- *
- * @param ship The galleon.
- */
-void movements_move_to_front(struct Ship ship);
 
 /**
  * @brief Move out of harm's way.
@@ -25,15 +18,31 @@ void movements_move_to_front(struct Ship ship);
  */
 void movements_flee(struct Ship ship);
 
-int movements_force(int x, int y, int mode);
-
+/**
+ * @brief Get the caravels containing some gold and send them home.
+ */
 void movements_retreive_gold(void);
 
+/**
+ * @brief Go get the gold from the richest volcano.
+ */
+void movements_get_higher_volcano_gold(void);
+
+/**
+ * @brief Send the caravels to colonize.
+ *
+ * @param d The maximum distance a caravel will try to travel to colonize.
+ */
 void movements_discover(int d);
 
+/**
+ * @brief Send the caravels to get the gold from volcanos.
+ */
 void movements_get_volcano_gold(void);
 
+/**
+ * @brief Move the galleons.
+ */
 void movements_move_galleons(void);
 
-void movements_get_higher_volcano_gold(void);
 #endif // __MOVEMENTS_H__
